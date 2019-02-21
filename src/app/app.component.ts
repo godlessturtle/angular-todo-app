561c229e7d6b80d9fc67eebafa050d51b83593b7
godlessturtle <murataslanx33@gmail.com>
2019-02-21 13:33:56 +0300

    service file generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StringsServiceService} from './strings-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'untitled7';
+export class AppComponent extends StringsServiceService {
+  todos = [];
+  dones = [];
+  msg = '';
+
+  // uygulama başlatıldığında local storage güncelle
+  ngOnInit() {
+    this.setTodos();
+  }
+
+  // yapılacaklar listesine ekleme
+  addTodo(todo) {
+    if (todo.value !== '') {
+      this.todos.push(todo.value);
+      todo.value = '';
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+      this.setTodos();
+      this.msg = '';
+    } else {
+      this.msg = 'Bu alan boş bırakılamaz';
+    }
+  }
+
+  // local storage object oluşturma ve güncelleme
+  setTodos() {
+    if (!localStorage.getItem('todos')) {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    } else {
+      this.todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    if (!localStorage.getItem('dones')) {
+      localStorage.setItem('dones', JSON.stringify(this.dones));
+    } else {
+      this.dones = JSON.parse(localStorage.getItem('dones'));
+    }
+
+  }
+
+  // yapılacaklar listesinden eleman kaldır ve localstorage de güncelle
+  removeTodo(todo) {
+    const index = this.todos.indexOf(todo.innerHTML);
+    this.deleteArrayItem(this.todos, index);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.msg = '';
+  }
+
+  // yapılacak listesindeki bir elemanı tamamlandı olarak işaretle
+  markAsDone(todo) {
+    const index = this.todos.indexOf(todo.innerHTML);
+    this.dones.push(todo.innerHTML);
+    this.deleteArrayItem(this.todos, index);
+
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    localStorage.setItem('dones', JSON.stringify(this.dones));
+    this.msg = '';
+  }
+
+  // tamamlananlar listesinden eleman kaldır ve localstorage de güncelle
+  removeDone(done) {
+    const index = this.dones.indexOf(done.innerHTML);
+    this.deleteArrayItem(this.dones, index);
+    localStorage.setItem('dones', JSON.stringify(this.dones));
+    this.msg = '';
+  }
+
+  // diziden belirtilen index numarasındaki elemanı sil
+  deleteArrayItem(arr, index) {
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material';
+
+import {SweetAlert2Module} from '@toverux/ngx-sweetalert2';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    SweetAlert2Module.forRoot({
+      buttonsStyling: false,
+      customClass: 'modal-content',
+      confirmButtonClass: 'btn btn-primary',
+      cancelButtonClass: 'btn'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
